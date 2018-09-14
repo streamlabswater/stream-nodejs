6c3900cd241fd29800f5a75f088168aeef13eae6
mospired <mo@mospired.com>
2018-09-14 16:50:27 -0400

    separate data module handler

---
+++ dal.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// const qs = require('querystring')
+const wreck = require('wreck')
+const { api: { host } } = require('../config')
+
+module.exports = async function (props = {}) {
+  const {
+    resource = '/hello/world',
+    method = 'GET',
+    payload,
+    oauth
+  } = props
+
+  const { id_token: token } = oauth
+  const url = `${host}/dev/v1${resource}`
+
+  const options = {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    payload
+  }
+
+  if (!payload) {
+    delete options.payload
+  }
+
+  try {
+    const response = await wreck.request(method, url, options)
+    const body = await wreck.read(response, { json: 'strict' })
+
+    return {
+      request: {
+        url: resource,
+        method,
+        body: payload
+      },
+      response: {
+        statusCode: response.statusCode,
+        statusMessage: response.statusMessage,
+        body
+      }
+    }
+  } catch (err) {
+    console.error(err)
+    return err
+  }
+}
--- routes/streamlabs.js
+++ routes/streamlabs.js
@@ -2,40 +2,89 @@
 
 const express = require('express')
 const router = express.Router()
-const api = require('../api')
+const dal = require('../dal')
 
-router.get('/', api({ resource: '/hello/world' }), (req, res, next) => {
-  const { payload } = res
-  return res.render('hello', {
-    title: 'hello',
-    payload
-  })
+router.use(function (req, res, next) {
+  const { oauth } = req.session
+
+  if (!oauth) {
+    return res.redirect('/')
+  }
+  next()
 })
 
-router.get('/:collection', api(), (req, res, next) => {
-  const { payload } = res
+router.get('/', async (req, res, next) => {
+  const { oauth } = req.session
+  const api = await dal({
+    resource: '/hello/world',
+    oauth
+  })
+
+  console.log('DATA', dal)
+
+  return res.render('hello', {
+    title: 'hello',
+    api
+  })
+})
+//
+router.get('/:collection', async (req, res, next) => {
   const collection = req.params.collection
   const identifier = `${collection.slice(0, -1)}Id`
+  const { oauth } = req.session
+  const api = await dal({
+    resource: req.path,
+    oauth
+  })
 
   return res.render('collection', {
     title: collection,
     collection,
     identifier,
-    payload
+    api
   })
 })
 
-router.get('/:collection/:itemId', api(), (req, res, next) => {
-  const { payload } = res
+router.get('/:collection/:itemId', async (req, res, next) => {
   const collection = req.params.collection
-  const identifier = `${collection.slice(0, -1)}Id`
+  const item = collection.slice(0, -1)
+  const identifier = `${item}Id`
+  const { oauth } = req.session
+  const api = await dal({
+    resource: req.path,
+    oauth
+  })
 
   return res.render('item', {
     title: `${collection}/${req.params.itemId}`,
     collection,
     identifier,
-    payload
+    item,
+    api
   })
 })
+
+router.post('/:collection/:itemId', async (req, res, next) => {
+  const collection = req.params.collection
+  const item = collection.slice(0, -1)
+  const identifier = `${item}Id`
+
+  const { oauth } = req.session
+  const api = await dal({
+    resource: req.path,
+    method: 'PUT',
+    payload: req.body,
+    oauth
+  })
+
+  return res.render('item', {
+    title: `${collection}/${req.params.itemId}`,
+    collection,
+    identifier,
+    item,
+    api
+  })
+})
+
 
 module.exports = router
